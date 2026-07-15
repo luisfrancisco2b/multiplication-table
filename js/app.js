b8a82aef86c9994450899e4f363567cb32b6769c
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-15 20:45:25 -0300

    feat(js): add initial logic for multiplication form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("teste")
+// Seleção de Elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+// Funções
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Número a ser multiplicado
+  const multiplicationNumber = +numberInput.value;
+
+  // Quantidade de vezes que o número vai ser multiplicado
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  console.log(multiplicationNumber, multiplicatorNumber);
+});
